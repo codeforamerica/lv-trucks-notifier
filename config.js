4bd56cfd62b8acbdb96b5e975ac758ebaa2ab2e0
Lou Huang <lou@louhuang.com>
2015-02-03 08:57:32 -0500

    Add a note about date strings to config.

--- config.js
+++ config.js
@@ -17,6 +17,12 @@ var config = {
   SENDGRID_USERNAME: process.env.SENDGRID_USERNAME,
   SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
 
+  // Note on valid strings for dates. These are parsed by moment.js
+  // http://momentjs.com/docs/#/parsing/
+  // Strings should be in ISO 8601 format, e.g. '2013-02-08'
+  // Other strings like 'December 31, 2014' will work but support for
+  // this is unreliable and may be deprecated in the future. See:
+  // https://github.com/moment/moment/issues/1407
   DATE_PROGRAM_START: '',
   DATE_PROGRAM_END: '',
 
